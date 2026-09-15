6e1664c3787d6ea3e68aeea4e9ccb699acb84bbd
Goitsemang-Mabuse <ST10505287@rcconnect.edu.za>
2026-09-15 21:00:23 +0200

    Initial project setup

---
+++ PROG5121wPOE/src/main/java/com/mycompany/prog5121wpoe/PROG5121wPOE.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prog5121wpoe;
+
+/**
+ *
+ * @author Shimm
+ */
+public class PROG5121wPOE {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
