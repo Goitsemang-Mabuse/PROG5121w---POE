8cd732e8d898bb961b25ae23ac18b5c92b4e73eb
Goitsemang-Mabuse <ST10505287@rcconnect.edu.za>
2026-09-17 21:14:49 +0200

    fixed a typo

--- PROG5121wPOE/src/main/java/com/mycompany/prog5121wpoe/PROG5121wPOE.java
+++ PROG5121wPOE/src/main/java/com/mycompany/prog5121wpoe/PROG5121wPOE.java
@@ -12,6 +12,8 @@ public class PROG5121wPOE {
 
     public static void main(String[] args) {
         Login login = new Login();
+        
+        //Check whether the system accepts a valid username amd rejects an invalid username
         System.out.println(login.checkUserName("kyl_1"));
         System.out.println(login.checkUserName("kyle!!!!!!"));
     }
