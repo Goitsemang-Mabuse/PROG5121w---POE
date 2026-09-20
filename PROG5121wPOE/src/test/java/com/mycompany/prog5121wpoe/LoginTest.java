89697760c675d98408729fcb101a1d80eccc4301
Goitsemang-Mabuse <ST10505287@rcconnect.edu.za>
2026-09-20 15:32:26 +0200

    Added JUnit tests

--- PROG5121wPOE/src/test/java/com/mycompany/prog5121wpoe/LoginTest.java
+++ PROG5121wPOE/src/test/java/com/mycompany/prog5121wpoe/LoginTest.java
@@ -28,4 +28,41 @@ public void testCheckUserName_invalid() {
     assertEquals(false, login.checkUserName("kyle!!!!!!"));
     }
 
+@Test
+public void testCheckPasswordComplexity_valid() {
+    Login login = new Login();
+    assertEquals(true, login.checkPasswordComplexity("Ch&&sec@ke99!"));
+    }
+
+@Test
+public void testCheckPasswordComplexity_invalid() {
+    Login login = new Login();
+    assertEquals(false, login.checkPasswordComplexity("password"));
+    }
+
+@Test
+public void testCheckCellPhoneNumber_valid() {
+    Login login = new Login();
+    assertEquals(true, login.checkCellPhoneNumber("+27838968976"));
+    }
+
+@Test
+public void testCheckCellPhoneNumber_invalid() {
+    Login login = new Login();
+    assertEquals(false, login.checkCellPhoneNumber("08966553"));
+    }
+
+@Test
+public void testLoginUser_successful(){
+    Login login = new Login();
+    login.registerUser("kyl_1", "Ch&&sec@ke99!", "+27838968976");
+    assertTrue(login.loginUser("kyl_1", "Ch&&sec@ke99!"));
+    }
+
+@Test
+public void testLoginUser_failed(){
+    Login login = new Login();
+    login.registerUser("kyl_1", "Ch&&sec@ke99!", "+27838968976");
+    assertFalse(login.loginUser("kyl_1", "wrongpassword"));
+    }
 }
